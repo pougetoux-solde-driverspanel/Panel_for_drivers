c2e951a4395e16b024e8fb34a7835a9c03448e25
Robin SOLDE <robin.solde@gmail.com>
2019-05-02 14:23:39 +0200

    loading image from file finally works

---
+++ drawImage/DefaultImages.h
@@ -0,0 +1,14 @@
+#ifndef Morse_h
+#define Morse_h
+    
+#include <Arduino.h>
+#include "smile.h"
+#include "thumbsDown.h"
+#include "thumbsUp.h"
+#include "UnHappySmile.h"
+#include "fuckYou.h"
+#include "neutralSmile.h"
+    
+
+    
+#endif
