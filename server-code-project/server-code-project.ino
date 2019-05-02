6d01db751b7829e976c6b73f65915811d826ede9
JeanPougetoux <spyroo913@hotmail.com>
2019-05-02 11:40:49 +0200

    adding gridcanvas

--- server-code-project/server-code-project.ino
+++ server-code-project/server-code-project.ino
@@ -11,13 +11,40 @@
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
+//---------------------------------------------------------------
+//Our HTML webpage contents in program memory
+const char MAIN_page[] PROGMEM = R"=====(
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script>
+  $(document).ready(function(){
+      $("p").click(function(){
+          $(this).hide();
+      });
+  });
+  </script>
+  </head>
+  <body>
+  
+  <p>If you click on me, I will disappear.</p>
+  <p>Click me away!</p>
+  <p>Click me too!</p>
+  <br><hr>
+  <a href="https://circuits4you.com">circuits4you.com</a>  
+  </body>
+  </html>
+)=====";
+//---------------------------------------------------------------
+
 ESP8266WebServer server(80);
 
 const int led = 13;
 
 void handleRoot() {
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  delay(1000);                      
+  delay(1000);
   server.send(200, "text/plain", "hello from esp8266!");
   digitalWrite(LED_BUILTIN, HIGH);
 }
@@ -40,7 +67,7 @@ void handleNotFound() {
 }
 
 void setup(void) {
-  pinMode(LED_BUILTIN, OUTPUT); 
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
@@ -67,6 +94,11 @@ void setup(void) {
 
   server.on("/inline", []() {
     server.send(200, "text/plain", "this works as well");
+  });
+
+  server.on("/mainpage", []() {
+    String s = MAIN_page;
+    server.send(200, "text/html", s);
   });
 
   server.onNotFound(handleNotFound);
