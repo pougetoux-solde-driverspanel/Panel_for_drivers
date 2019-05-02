744c948ab379a6677ce7ba7ddda8f7d653ea3eaa
JeanPougetoux <spyroo913@hotmail.com>
2019-05-02 15:33:34 +0200

    ok for post calls

--- server-code-project/server-code-project.ino
+++ server-code-project/server-code-project.ino
@@ -18,7 +18,7 @@ const char MAIN_page[] PROGMEM = R"=====(
   <html>
       <head>
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
           <script type="text/javascript" language="javascript">
@@ -78,10 +78,11 @@ const char MAIN_page[] PROGMEM = R"=====(
               }
   
               function selectoption(button) {
+                  console.log(button);
                   $.ajax({
-                      url: "",
-                  }).done(function(data) {
-                      
+                      type: "POST",
+                      url: "http://192.168.43.38/call",
+                      data: {"selected": button}
                   });
               }
           </script>
@@ -245,6 +246,27 @@ void setup(void) {
   server.on("/mainpage", []() {
     String s = MAIN_page;
     server.send(200, "text/html", s);
+  });
+
+  server.on("/call", HTTP_POST, []() {
+    if(server.arg(0) == "1") {
+      Serial.println("ici 1");
+    }
+    else if(server.arg(0) == "2") {
+      Serial.println("ici 2");
+    }
+    else if(server.arg(0) == "3") {
+      Serial.println("ici 3");
+    }
+    else if(server.arg(0) == "4") {
+      Serial.println("ici 4");
+    }
+    else if(server.arg(0) == "5") {
+      Serial.println("ici 5");
+    }
+    else if(server.arg(0) == "6") {
+      Serial.println("ici 6");
+    }
   });
 
   server.onNotFound(handleNotFound);
